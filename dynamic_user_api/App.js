32418de100dd9c6aafd0d879b60c6c5811ae9c14
ahurara <hurrerahchamp57@gmail.com>
2024-03-06 23:34:15 +0500

    added some concepts related to core components and layout and responsive designs

--- dynamic_user_api/App.js
+++ dynamic_user_api/App.js
@@ -1,23 +1,31 @@
-import { StyleSheet, Text, View , Dimensions ,useWindowDimensions} from 'react-native';
+import { StyleSheet, Text, View , Dimensions ,useWindowDimensions , SafeAreaView ,StatusBar ,Platform} from 'react-native';
 
+
+// safeAreaView is used to make the element not hide under the nothc or upper side of the screen
 export default function App() {
 
+  //drawback of dimension api is that it does'nt update dynamically based on changes in window dimension (potrait to landscap)
+  // const windowWidth = Dimensions.get("window").width;
+  // const windowHeight = Dimensions.get("window").height;
+  
   const { width, height } = useWindowDimensions();
 
   return (
+
+    <SafeAreaView style={styles.safeArea}>
     <View style={styles.container}>
+    <Text style={[{fontSize:22},styles.text]}>Welcome </Text>
       <View style={[styles.box,
       { width: width>500 ? '70%' : '90%',
         height: height>600 ?'60%' : '90%',}]}>
       <Text style={{fontSize: width> 500 ? 50 : 24,}}>Hello</Text>
       </View>
     </View>
+    </SafeAreaView>
   );
+
 }
 
-  //drawback of dimension api is that it does'nt update dynamically based on changes in window dimension (potrait to landscap)
-  const windowWidth = Dimensions.get("window").width;
-  const windowHeight = Dimensions.get("window").height;
   
 
 
@@ -26,12 +34,39 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'plum',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
+    
+    //this line gives the paddig only to the android device
+    //this line is used to change a single style property 
+    paddingTop: Platform.OS === "android" ? 25 : 0
   },
   box:{
     backgroundColor:"lightblue",
     alignItems:'center',
     justifyContent:'center',
   },
+  safeArea:{
+    flex:1,
+    backgroundColor:'red',
+    marginTop:StatusBar.currentHeight
+    
+  },
+  text :{
+     ...Platform.select({
+      ios:{
+        color:'purple'
+      },
+      android:{
+        color:'grey',
+        fontSize:29
+      },
+      
+    }),
+    fontWeight:'bold',
+  }
  
 });
+
+/* u can also code separately for the devices (android / ios) just save the components like (button.ios.js and button.android.js)
+reactnative will automatically execute the code based on the platform u using
+*/
--- flex_prac/App.js
+++ flex_prac/App.js
@@ -8,18 +8,18 @@ export default function App() {
       <Box style={{backgroundColor:'#b65d1f'}}>Box 2</Box>
       <Box style={{backgroundColor:'#1c4c56', flexGrow:3}}>Box 3</Box>
       <Box style={{backgroundColor:'#ab9156', flexGrow:1}}>Box 4</Box>
-      <Box style={{backgroundColor:'#6b0803'}}>Box 5</Box>
-      <Box style={{backgroundColor:'#1c4c56'}}>Box 6</Box>
-      <Box style={{backgroundColor:'#b95f21'}}>Box 7</Box>
+      {/* <Box style={{backgroundColor:'#6b0803' , alignSelf:'center'}}>Box 5</Box> */}
+      {/* <Box style={{backgroundColor:'#1c4c56'}}>Box 6</Box>
+      <Box style={{backgroundColor:'#b95f21'}}>Box 7</Box> */}
     </View>
   );
 }
 {
   //flexDirection (column = top to bottom) (coloumn-reverse = bottom to top ) (row = left to right) (row-reverse = right to left)
   
-  //justifyContent (flex-start = oject at the start ) (flex-end = object placed at the end)
+  //justifyContent (flex-start = oject at the start ) (flex-end = object placed at the end) w.r.t the flex direction 
   
-  //alignItem is applied to the container and control the alignment of all the itens inside it
+  //alignItem is applied to the container and control the alignment of all the itens inside it. (Similar to justify content but works perpendicular)
 
   //aligSelf is applied to the individual item and control the alignment of it 
 
@@ -30,6 +30,9 @@ export default function App() {
   //use flexshrink when the flex is smaller then the item it has this property will divide the space with the item inside flex
 
   //use flexGrow when the flex area is larger then the item it has so item takes the remaining space
+
+  /*alignItems aligns flex items along the cross-axis of the flex container on a single line, while alignConten
+  t aligns multiple lines of flex items within the container. */
 }
 
 
@@ -38,13 +41,13 @@ const styles = StyleSheet.create({
    marginTop:64,
    borderWidth:6,
    borderColor:'red',
-   flex:1,
+    //flex:1,
    flexDirection:"column",
    justifyContent:"flex-start",
-   alignItems:"stretcht",
-  //  height:300,
+   alignContent:"center",
+    height:300,
   //  width:200,
-  //  flexWrap:'wrap',
+   flexWrap:'wrap',
   //  rowGap:20,
   //  columnGap:20
   // gap:10
